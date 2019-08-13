acd3327071b1c396b1748bdcd55d0f2b6af28935
Benjamin Grand <b.grand.hansen@gmail.com>
2019-08-13 10:48:29 +0200

    Exercise data binding done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import { ImageComponentComponent } from './image-component/image-component.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    ImageComponentComponent
 
   ],
   imports: [
---
+++ src/app/image-component/image-component.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-component',
+  templateUrl: './image-component.component.html',
+  styleUrls: ['./image-component.component.css']
+})
+export class ImageComponentComponent implements OnInit{
+    imageLink;
+    constructor() { }
+
+    ngOnInit() {
+
+    }
+}
+
